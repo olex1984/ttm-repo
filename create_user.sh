706b3c78ce41af2dc06f55f3e8128af907df5a6d
root <oleg.zitzer@gmail.com>
2020-08-01 19:34:40 +0000

    cr

--- create_user.sh
+++ create_user.sh
@@ -1,1 +1,2 @@
-curl --header "Private-Token: MmczjSrRgF8jKEWqx4f2" -d "email=test1@test.org&name=Firstname%20Lastname&username=test_username1&password=1234567890" "http://ttm-gitlab/api/v4/users"
+#!/bin/bash
+curl --header "Private-Token: MmczjSrRgF8jKEWqx4f2" -d "email=test_final@test.org&name=Firstname%20Lastname&username=test_username_final&password=1234567890" "http://ttm-gitlab/api/v4/users"
